aa1a476dec1b86d7920b4032ebf6214571343c0f
Yaroslav Grachev <y.grachev@rbspayment.ru>
2021-03-21 22:04:00 +0300

    Simple file handler

--- src/components/pages/start/canvas-drop/canvas-drop.tsx
+++ src/components/pages/start/canvas-drop/canvas-drop.tsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { ChangeEvent, HTMLAttributes } from 'react';
 import cn from 'clsx';
 
 import addPhoto from '@images/icons/add_photo.svg';
 import styles from './canvas-drop.module.scss';
 
-function CanvasDrop({ className }: React.HTMLAttributes<HTMLDivElement>) {
+function CanvasDrop({ className }: HTMLAttributes<HTMLDivElement>) {
+  const handleFile = (event: ChangeEvent<HTMLInputElement>) => {
+    const x = event.target.files;
+    console.log('🚀 ~ file: canvas-drop.tsx ~ line 10 ~ handleFile ~ x', x);
+  };
+
   return (
     <section className={styles.dropContainer}>
-      {/* <input type="file" name="" id=""/> */}
       <canvas className={cn(styles.canvas, className)}></canvas>
       <div className={styles.helperArea}>
-        <button className={styles.btn} type="button">
-          <img className={styles.image} src={addPhoto} alt="" />
-          <p className={styles.text}>Drop or paste</p>
-        </button>
+        <input
+          id="file"
+          className={styles.fileInput}
+          type="file"
+          accept="image/*"
+          onChange={handleFile}
+        />
+        <label className={styles.fileLabel} htmlFor="file">
+          <img className={styles.image} src={addPhoto} alt="" width="70" height="65" />
+          <p className={styles.text}>Drop image</p>
+          {/* <p className={styles.text}>Drop or paste</p> */}
+        </label>
       </div>
     </section>
   );
